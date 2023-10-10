8c4e585ec35536279eaafddc3508cbffca40bc27
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-10-10 16:39:08 +0530

    added ABout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,8 @@ import {
   Box,
   IconButton,
   useBreakpointValue,
-  Stack,
-  Heading,
-  Text,
-  Container,
 } from '@chakra-ui/react'
+import { AspectRatio } from '@chakra-ui/react'
 // Here we have used react-icons package for the icons
 import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi'
 // And react-slick as our Carousel Lib
@@ -23,7 +20,7 @@ const settings = {
   infinite: true,
   autoplay: true,
   speed: 500,
-  autoplaySpeed: 5000,
+  autoplaySpeed: 3000,
   slidesToShow: 1,
   slidesToScroll: 1,
 }
@@ -35,34 +32,30 @@ export default function About() {
 
   // These are the breakpoints which changes the position of the
   // buttons as the screen size changes
-  const top = useBreakpointValue({ base: '90%', md: '50%' })
-  const side = useBreakpointValue({ base: '30%', md: '40px' })
+  const top = useBreakpointValue({ base: '60%', md: '50%' })
+  const side = useBreakpointValue({ base: '10%', md: '40px' })
 
   // This list contains all the data for carousels
   // This can be static or loaded from a server
   const cards = [
     {
-      title: 'Design Projects 1',
-      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+
       image:
         'https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmlrZSUyMHNob2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
     },
     {
-      title: 'Design Projects 2',
-      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+
       image:
         'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
     },
     {
-      title: 'Design Projects 3',
-      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
         'https://t3.ftcdn.net/jpg/02/09/82/26/360_F_209822653_Fzoff04bZ8QHnseFCHy7vkLmWl1mIDHO.jpg',
     },
   ]
 
   return (
-    <Box position={'relative'} height={'1000px'} width={'full'} overflow={'hidden'}>
+    <Box position={'relative'} height={'500px'} width={'full'} overflow={'hidden'}>
       {/* CSS files for react-slick */}
       <link
         rel="stylesheet"
@@ -82,7 +75,7 @@ export default function About() {
         left={side}
         top={top}
         transform={'translate(0%, -50%)'}
-        zIndex={2}
+        zIndex={1}
         onClick={() => slider?.slickPrev()}>
         <BiLeftArrowAlt size="40px" />
       </IconButton>
@@ -94,39 +87,24 @@ export default function About() {
         right={side}
         top={top}
         transform={'translate(0%, -50%)'}
-        zIndex={2}
+        zIndex={1}
         onClick={() => slider?.slickNext()}>
         <BiRightArrowAlt size="40px" />
       </IconButton>
       {/* Slider */}
       <Slider {...settings} ref={(slider) => setSlider(slider)}>
         {cards.map((card, index) => (
-          <Box
-            key={index}
-            height={'6xl'}
-            position="relative"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            backgroundImage={`url(${card.image})`}>
-            {/* This is the block you need to change, to customize the caption */}
-            <Container size="container.lg" height="600px" position="relative">
-              <Stack
-                spacing={6}
-                w={'full'}
-                maxW={'lg'}
-                position="absolute"
-                top="50%"
-                transform="translate(0, -50%)">
-                <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }} color={'gray'}>
-                  {card.title}
-                </Heading>
-                <Text fontSize={{ base: 'md', lg: 'lg' }} color="purple">
-                  {card.text}
-                </Text>
-              </Stack>
-            </Container>
-          </Box>
+          <AspectRatio key={index} maxW='700px' ratio={4 / 3}>
+            <Box
+
+              // height={'6xl'}
+              position="relative"
+              backgroundPosition="center"
+              backgroundRepeat="no-repeat"
+              backgroundSize="cover"
+              backgroundImage={`url(${card.image})`}>
+            </Box>
+          </AspectRatio>
         ))}
       </Slider>
     </Box>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,53 @@
-import About from "./About";
+
 // import Navbar from "../components/Navbar";
+import {
+  Flex,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  Button,
+} from '@chakra-ui/react'
+import About from "./About";
 function HomePage() {
-  return (
-   <About/>
-  );
+    return (
+      <Container maxW={'5xl'}>
+        <Stack
+          textAlign={'center'}
+          align={'center'}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Enjoy Shopping {' '}
+            <Text as={'span'} color={'#3c0080'}>
+             All New Shoes
+            </Text>
+          </Heading>
+          <Text color={'gray.500'} maxW={'3xl'}>
+        Checkout 
+          </Text>
+          <Stack spacing={6} direction={'row'}>
+            <Button
+              rounded={'full'}
+              px={6}
+              colorScheme={'orange'}
+              bg={'#3c0080'}
+              _hover={{ bg:' #5d259e '}}>
+              Get started
+            </Button>
+            <Button rounded={'full'} px={6}>
+              Learn more
+            </Button>
+          </Stack>
+          <Flex w={'full'}>
+            <About/>
+          </Flex>
+        </Stack>
+      </Container>
+    )
 }
 
 export default HomePage;
