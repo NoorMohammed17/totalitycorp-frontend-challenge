6602610fe4d77c23611a6c3bccdaa22cdb0d2038
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-10-11 13:04:01 +0530

    added new input

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useSearchParams, useLocation } from 'react-router-dom';
-import { SimpleGrid, Spinner } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Flex } from "@chakra-ui/react";
+import { Input } from '@chakra-ui/react'
 import axios from 'axios';
 import ProductCard from "./ProductCard";
 //import data from '../../data/data';
@@ -13,6 +14,7 @@ const url = 'https://lazy-mite-cardigan.cyclic.app/products'
 
 const Products = (props) => {
   const [products, setProducts] = useState([]);
+  const [query, setQuery] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams] = useSearchParams();
   const location = useLocation()
@@ -24,13 +26,14 @@ const Products = (props) => {
       color: searchParams.getAll('color'),
       _sort: searchParams.get('order') && 'newPrice',
       _order: searchParams.get('order'),
+      // title_like:query,
     }
   }
 
 
 
   const productsFetchHandler = (paramObj) => {
-   
+
     axios
       .get(url, paramObj)
       .then((res) => {
@@ -60,13 +63,28 @@ const Products = (props) => {
       size='xl'
     />
   }
+  const inputChangeHandler = (e) => {
+    setQuery(e.target.value)
+    console.log(query)
+  }
 
 
 
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <SimpleGrid columns={[1, 2, 2, 3, 4]} spacing='40px'>
+      <Flex width={{ md: '30%', base: '80%' }} marginBottom={'20px'}>
+        <Input
+          color='#3c0080'
+          placeholder='Search your favorite products'
+          _placeholder={{ color: 'inherit' }}
+          onChange={inputChangeHandler}
+       
+
+        />
+      </Flex>
+
+      <SimpleGrid columns={[1, 2, 2, 3, 4]} spacing={{ base: '15px', md: '30px' }}>
         {products.map((product) => (
           <ProductCard
             key={product.id}
