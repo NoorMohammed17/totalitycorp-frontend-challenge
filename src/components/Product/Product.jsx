9798b8090be5889cae2433c883242c295d9ccde4
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-10-14 11:13:31 +0530

    added input

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,20 +1,16 @@
 import { useState, useEffect } from 'react';
-import { useSearchParams, useLocation } from 'react-router-dom';
-import { SimpleGrid, Spinner, Flex } from "@chakra-ui/react";
-import { Input } from '@chakra-ui/react'
 import axios from 'axios';
+import { useSearchParams, useLocation } from 'react-router-dom';
+import { SimpleGrid, Spinner } from "@chakra-ui/react";
+
 import ProductCard from "./ProductCard";
 //import data from '../../data/data';
 import classes from "./Product.module.css";
-
-
-
 
 const url = 'https://lazy-mite-cardigan.cyclic.app/products'
 
 const Products = (props) => {
   const [products, setProducts] = useState([]);
-  const [query, setQuery] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams] = useSearchParams();
   const location = useLocation()
@@ -26,7 +22,7 @@ const Products = (props) => {
       color: searchParams.getAll('color'),
       _sort: searchParams.get('order') && 'newPrice',
       _order: searchParams.get('order'),
-      // title_like:query,
+      title_like:props.onInput,
     }
   }
 
@@ -63,27 +59,12 @@ const Products = (props) => {
       size='xl'
     />
   }
-  const inputChangeHandler = (e) => {
-    setQuery(e.target.value)
-    console.log(query)
-  }
 
 
 
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <Flex width={{ md: '30%', base: '80%' }} marginBottom={'20px'}>
-        <Input
-          color='#3c0080'
-          placeholder='Search your favorite products'
-          _placeholder={{ color: 'inherit' }}
-          onChange={inputChangeHandler}
-       
-
-        />
-      </Flex>
-
       <SimpleGrid columns={[1, 2, 2, 3, 4]} spacing={{ base: '15px', md: '30px' }}>
         {products.map((product) => (
           <ProductCard
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { Stack, Box,Divider } from '@chakra-ui/react';
 import Sidebar from '../components/SideBar';
 import Products from '../components/Product/Product';
+import { Input ,Flex} from '@chakra-ui/react'
 
 const ProductsPage = () => {
+  const [query, setQuery] =React. useState("");
+  const inputChangeHandler = (e) => {
+    setQuery(e.target.value)
+    console.log(query)
+  }
+
   return (
    
     <Stack direction={['column','column','row', 'row']}>
@@ -15,7 +22,17 @@ const ProductsPage = () => {
     <Divider borderColor={'red'}  orientation='vertical'/>
 
     <Box>
-      <Products />
+    <Flex width={{ md: '30%', base: '80%' }} marginTop={'20px'}>
+        <Input
+          color='#3c0080'
+          placeholder='Search your favorite products'
+          _placeholder={{ color: 'inherit' }}
+          onChange={inputChangeHandler}
+       
+
+        />
+      </Flex>
+      <Products  onInput={query}/>
     </Box>
 
   </Stack>
